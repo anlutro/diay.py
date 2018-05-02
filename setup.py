c5d56834d3c0df24bed4e81b18d08fb942222709
Andreas Lutro <anlutro@gmail.com>
2018-05-02 19:04:43 +0200

    oops, tabs

--- setup.py
+++ setup.py
@@ -7,25 +7,25 @@ from setuptools import setup
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
-	name='diay',
-	version='0.1.2',
-	license='MIT',
-	description='diay - a dependency injection library',
-	long_description=open('README.md').read(),
-	long_description_content_type='text/markdown',
-	author='Andreas Lutro',
-	author_email='anlutro@gmail.com',
-	url='https://github.com/anlutro/diay.py',
-	# packages does not work for single file modules
-	py_modules=['diay'],
-	classifiers=[
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'Operating System :: POSIX',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-	],
-	keywords=['dependency injection', 'inversion of control'],
+    name='diay',
+    version='0.1.2',
+    license='MIT',
+    description='diay - a dependency injection library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Andreas Lutro',
+    author_email='anlutro@gmail.com',
+    url='https://github.com/anlutro/diay.py',
+    # packages does not work for single file modules
+    py_modules=['diay'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=['dependency injection', 'inversion of control'],
 )
