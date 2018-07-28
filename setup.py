bd8a3cdaaf5c723220578a696367dfc560e4c6c9
Andreas Lutro <anlutro@gmail.com>
2018-07-28 14:20:14 +0200

    version 0.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='diay',
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     description='diay - a dependency injection library',
     long_description=open('README.md').read(),
