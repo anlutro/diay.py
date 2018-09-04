653315763cc147b2b71f454faab6c647f024b3d0
Andreas Lutro <anlutro@gmail.com>
2018-09-04 16:36:57 +0200

    add failing test with new desired behavior

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -6,6 +6,36 @@ def test_plugin():
         @diay.provider
         def f(self) -> str:
             return 'foo'
+
     i = diay.Injector()
     i.register_plugin(TestPlugin())
+    assert 'foo' == i.get(str)
+
+
+def test_plugin_lazy():
+    """
+    TestPlugin has a dependency which needs a provider, but whether we
+    register the plugin or provider first should not matter. The plugin should
+    be lazily instantiated.
+    """
+    class Dep:
+        def __init__(self, v):
+            self.v = v
+
+        def f(self):
+            return self.v
+
+    class TestPlugin(diay.Plugin):
+        def __init__(self, dep: Dep):
+            self.dep = dep
+
+        @diay.provider
+        def f(self) -> str:
+            return self.dep.f()
+
+    i = diay.Injector()
+    i.register_plugin(TestPlugin)
+    @i.provider
+    def provide_dep() -> Dep:
+        return Dep('foo')
     assert 'foo' == i.get(str)
